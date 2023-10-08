5ee249aa83c92d54b7e2041b235bce1e3488cba0
Zekaria Bouchama <zekaria.bouchama@polymtl.ca>
2023-10-07 23:37:21 -0400

    version 90%

--- tp1/Client/src/Client.java
+++ tp1/Client/src/Client.java
@@ -3,133 +3,223 @@ import java.net.*;
 import java.nio.file.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
 import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-	
+
 	private static Socket socket;
-	
-	private static final String IP_ADDRESS_PATTERN =
-	        "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+	private static String serverAddress = null;
+	private static int serverPort = 0;
+	private static Scanner scanner = new Scanner(System.in);
+	private static DataOutputStream out;
+
+	private static final String IP_ADDRESS_PATTERN = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 
 	private static final Pattern pattern = Pattern.compile(IP_ADDRESS_PATTERN);
 
-    public static boolean validateIPAddress(String ipAddress) {
-        Matcher matcher = pattern.matcher(ipAddress);
-        return matcher.matches();
-    }
-    
-    public static boolean validatePort(int port) {
-        if( port>=5000 && port<=5050 )
-        	return true ;
-        return false ;
-    }
-	
-	public static void main(String[] args) throws Exception
-	{
-		
-		boolean isValid = false ;
-		Scanner scanner = new Scanner(System.in);
-		String serverAddress = null;
-        int serverPort = 0;
-			while (!isValid) {
-				
-		        System.out.print("Veuillez entrer l'addresse IP du serveur : \n");
-		        serverAddress = scanner.nextLine();
-		        if(validateIPAddress(serverAddress))
-		        {
-		        	isValid = true;
-		        }
-		        else {
-			        System.out.print("Addresse IP est invalide! \n");
+	public static boolean validateIPAddress(String ipAddress) {
+		Matcher matcher = pattern.matcher(ipAddress);
+		return matcher.matches();
+	}
 
-		        }
-		        
+	public static boolean validatePort(int port) {
+		if (port >= 5000 && port <= 5050)
+			return true;
+		return false;
+	}
+
+	static void readServerInfo() {
+		boolean isValid = false;
+		while (!isValid) {
+
+			System.out.print("Veuillez entrer l'addresse IP du serveur : \n");
+			serverAddress = scanner.nextLine();
+			if (validateIPAddress(serverAddress)) {
+				isValid = true;
+			} else {
+				System.out.print("Addresse IP est invalide! \n");
+
 			}
-			isValid = false ;
-			while (!isValid) {
-				System.out.print("Veuillez entrer le numero de port du serveur: \n");
-		        serverPort = scanner.nextInt();
-		        if(validatePort(serverPort)) {
-		        	isValid = true ;
-		        }
-		        else {
-		        	System.out.print("Numero de port est invalide! \n");
-		        }
-		        
+
+		}
+		isValid = false;
+		while (!isValid) {
+			System.out.print("Veuillez entrer le numero de port du serveur: \n");
+			serverPort = scanner.nextInt();
+			if (validatePort(serverPort)) {
+				isValid = true;
+			} else {
+				System.out.print("Numero de port est invalide! \n");
 			}
+
+		}
+
+	}
+
+	static boolean loggingToServer() {
+
+		try {
 			socket = new Socket(serverAddress, serverPort);
-			System.out.format("Connexion au serveur %s:%d%n",serverAddress, serverPort);
+			out = new DataOutputStream(socket.getOutputStream());
+			System.out.format("Connexion au serveur %s:%d%n", serverAddress, serverPort);
 			scanner.nextLine(); // Consume the newline character
+		} catch (IOException e) {
+			// Handle other IO-related exceptions
+			e.printStackTrace();
+			return false;
+		}
 
-	        System.out.print("Veuillez entrer votre nom d'utilisateur : \n");
-	        String username = scanner.nextLine();
+		System.out.print("Veuillez entrer votre nom d'utilisateur : \n");
+		String username = scanner.nextLine();
 
-	        System.out.print("Veuillez entrer votre mot de passe : \n");
-	        String password = scanner.nextLine();
-			
-	        // Send username and password to the server
-	        
-	        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-	        out.writeUTF(username);
-	        out.writeUTF(password);
-				
-			
-			
+		System.out.print("Veuillez entrer votre mot de passe : \n");
+		String password = scanner.nextLine();
+
+		// Send username and password to the server
+		try {
+			out.writeUTF(username);
+			out.writeUTF(password);
+		} catch (IOException e) {
+			System.out.println("Could not send username and password to the server");
+			return false;
+		}
+		String serverAnswer = null;
+		try {
 			DataInputStream in = new DataInputStream(socket.getInputStream());
-	        String response = in.readUTF();
-	        
-	        
-			// Process the response
-	        if (response.equals("SUCCEE")) {
-	            System.out.println("Authentification reussite.\n");
-				
-	        }
-	        else if(response.equals("NOUVEAU")) {
-	        	System.out.println("Creation d'un nouveau utilisateur.\n");
-	        } 
-	        else {
-	            System.out.println("Erreur dans la saisie du mot de passe\n Connexion rejetee");
-	            socket.close(); // Close the socket if authentication fails
-	        }
-	        System.out.println("COnnexion au service de traitment d'image.\n");
-			System.out.print("Veuillez entrer le nom de l'image que vous voulez l'envoyer : \n");
-	        String imageName = scanner.nextLine();
-	        
-	        String imagePath = Paths.get(imageName).toAbsolutePath().toString();
-	        System.out.format("image path : %s\n",imagePath);
-	        
-	        
-	        File file = new File(imagePath);
-	        System.out.println(!file.exists());
-	        imageName = scanner.nextLine();
-//
-//            if (!file.exists()) {
-//                System.out.println("Image non trouvee!");
-//                return;
-//            }
-//
-//            // Read the image file as binary data
-//            out.writeUTF(imageName);
-//            byte[] imageData = new byte[(int) file.length()];
-//            FileInputStream fis = new FileInputStream(file);
-//            fis.read(imageData);
-//            fis.close();
-//            try {
-//            	out.write(imageData);
-//            }
-//            catch (IOException e) {
-//            	System.out.println("Could not send Image") ;
-//            }
-//            System.out.println("Image was sent successfully to the server") ;
-//            out.flush();
-//            out.close();
-//            
-//            
-//            
-//            
-			scanner.close();
-			socket.close();
+			serverAnswer = in.readUTF();
+
+			// verify the User information
+			loggingVerification(serverAnswer);
+		} catch (IOException e) {
+			System.out.println("Could not read an answer from the server");
+			return false;
+		}
+		return true;
+	}
+
+	static void loggingVerification(String answer) {
+		if (answer.equals("SUCCESS")) {
+			System.out.println("Authentication successful.\n");
+
+		} else if (answer.equals("NEW")) {
+			System.out.println("Authentication successful.\n");
+		} else {
+			System.out.println("Error in password entry.\n Connection rejected");
+
+			try {
+				socket.close();// Close the socket if authentication fails
+			} catch (IOException e) {
+				System.out.println("Could not close the socket");
+			}
+		}
+		System.out.println("Connexion au service de traitment d'image.\n");
+	}
+
+	static Object[] readImage() {
+		Object[] result = new Object[2];
+		System.out.print("Veuillez entrer le nom de l'image que vous voulez l'envoyer : \n");
+		String imageName = scanner.nextLine();
+		result[0] = imageName;
+
+		String imagePath = Paths.get(imageName).toAbsolutePath().toString();
+
+		File file = new File(imagePath);
+		result[1] = file;
+		return result;
+	}
+
+	static void sendImagetoServer(String imageName, File file) {
+		try {
+			// Send the image file name to the server
+			out.writeUTF(imageName);
+			// Read the image file as binary data
+			byte[] imageData = new byte[(int) file.length()];
+			try {
+
+				FileInputStream fis = new FileInputStream(file);
+				fis.read(imageData);
+
+				// Send the length of the image data first
+				out.writeInt(imageData.length);
+
+				// Send the image data
+				out.write(imageData);
+				System.out.println("Image was sent successfully to the server");
+				fis.close();
+			} catch (IOException e) {
+				System.out.println("Could not send Image");
+			}
+
+		} catch (IOException e) {
+			System.out.println("Could not send Image name");
+		}
+	}
+
+	static void readSobelImage() {
+		try {
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+
+			String sobelImageName = in.readUTF();
+			int imageDataLength = in.readInt();
+			byte[] imageData = new byte[imageDataLength];
+			in.readFully(imageData);
+			System.out.println("Sobel image received from the server");
+
+			// Construct the full path of the Sobel image
+			File sobelImageFile = new File(sobelImageName);
+			String sobelImagePath = sobelImageFile.getAbsolutePath();
+			// Get the parent directory (folder) path
+			String parentFolderPath = new File(sobelImagePath).getParent();
+
+			// Save the new sobel image to a file
+			try {
+				FileOutputStream fos = new FileOutputStream(sobelImageFile);
+				fos.write(imageData);
+				fos.close();
+
+				System.out.println("Sobel image saved as: " + sobelImageName + " in " + parentFolderPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Could not read an answer from the server");
+		}
+	}
+
+	// this function is used to hanlde all image operations between the client and
+	// the server
+	static void ImageHandler() {
+
+		// reading image from the user
+		Object[] imageInfo = readImage();
+
+		// sending image to the server
+		sendImagetoServer((String) imageInfo[0], (File) imageInfo[1]);
+
+		// receiving sobel image from the server
+		readSobelImage();
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		// enrtering server information
+		readServerInfo();
+
+		// logging to the server
+		if (loggingToServer()) {
+			// sending image to the server
+			ImageHandler();
+		}
+
+		out.flush();
+		out.close();
+		scanner.close();
+		socket.close();
 	}
 }
--- tp1/Server/src/Server.java
+++ tp1/Server/src/Server.java
@@ -1,218 +1,303 @@
 
 import java.net.*;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Server 
-{
-	
+import java.awt.image.BufferedImage;
+
+public class Server {
+
 	private static HashMap<String, String> usersDatabase = new HashMap<>();
 	private static ServerSocket listener;
-	private static final String IP_ADDRESS_PATTERN =
-	        "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+	private static final String IP_ADDRESS_PATTERN = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 	private static final Pattern pattern = Pattern.compile(IP_ADDRESS_PATTERN);
+	private static int serverPort = 0;
+	private static String serverAddress = null;
 
-	
 	public static boolean validateUser(String username, String password) {
-		
-        // Vérifie si l'utilisateur existe et si le mot de passe correspond
-        
+
+		// Vérifie si l'utilisateur existe et si le mot de passe correspond
+
 		String storedPassword = usersDatabase.get(username);
-        return storedPassword != null && storedPassword.equals(password);
-    }
-	
-    public static boolean validateIPAddress(String serverAddress) {
-    	
-        Matcher matcher = pattern.matcher(serverAddress);
-        return matcher.matches();
-    }
-    
-    public static boolean validatePort(int serverPort) {
-        if( serverPort>=5000 && serverPort<=5050 )
-        	return true ;
-        return false ;
-    }
-    
-    public static void loadDatabaseFromCSV() {
-	    try (BufferedReader reader = new BufferedReader(new FileReader("user_database.csv"))) {
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	            String[] parts = line.split(",");
-	            if (parts.length == 2) {
-	                usersDatabase.put(parts[0], parts[1]);
-	            }
-	        }
-	        System.out.println("Database loaded from CSV.");
-	    } catch (IOException e) {
-	        System.err.println("Error loading database from CSV: " + e.getMessage());
-	    }
+		return storedPassword != null && storedPassword.equals(password);
 	}
-    
-        
-	
-	public static void main(String[] args) throws Exception 
-	{
-		
-		int clientNumber= 0 ;
-		
-		
-		boolean isValid = false ;
+
+	public static boolean validateIPAddress(String serverAddress) {
+
+		Matcher matcher = pattern.matcher(serverAddress);
+		return matcher.matches();
+	}
+
+	public static boolean validatePort(int serverPort) {
+		if (serverPort >= 5000 && serverPort <= 5050)
+			return true;
+		return false;
+	}
+
+	private static void loadDatabaseFromCSV() {
+
+		String dataBasePath = Paths.get("user_database.csv").toAbsolutePath().toString();
+		try (BufferedReader reader = new BufferedReader(new FileReader(dataBasePath))) {
+			System.out.println("Loading database from CSV: " + dataBasePath);
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(",");
+				if (parts.length == 2) {
+					usersDatabase.put(parts[0], parts[1]);
+				}
+			}
+			System.out.println("Database loaded from CSV.");
+		} catch (IOException e) {
+			System.err.println("Error loading database from CSV: " + e.getMessage());
+		}
+	}
+
+	private static void creatingServer() {
+
+		boolean isValid = false;
 		Scanner scanner = new Scanner(System.in);
 		String serverAddress = null;
-        int serverPort = 0;
-		
-		while (!isValid) {
-			
-	        System.out.print("Veuillez entrer l'addresse IP du poste sur lequel s'éxecute le serveur  : \n");
-	        serverAddress = scanner.nextLine();
-	        if(validateIPAddress(serverAddress))
-	        {
-	        	isValid = true;
-	        }
-	        else {
-		        System.out.print("Addresse IP est invalide! \n");
+		int serverPort = 0;
 
-	        }
-	        
+		while (!isValid) {
+
+			System.out.print("Veuillez entrer l'addresse IP du poste sur lequel s'éxecute le serveur  : \n");
+			serverAddress = scanner.nextLine();
+			if (validateIPAddress(serverAddress)) {
+				isValid = true;
+			} else {
+				System.out.print("Addresse IP est invalide! \n");
+
+			}
+
 		}
-		isValid = false ;
+		isValid = false;
 		while (!isValid) {
 			System.out.print("Veuillez entrer le numero de port d’écoute: \n");
-	        serverPort = scanner.nextInt();
-	        if(validatePort(serverPort)) {
-	        	isValid = true ;
-	        }
-	        else {
-	        	System.out.print("Numero de port est invalide! \n");
-	        }
-	        
+			serverPort = scanner.nextInt();
+			if (validatePort(serverPort)) {
+				isValid = true;
+			} else {
+				System.out.print("Numero de port est invalide! \n");
+			}
+
 		}
 		scanner.close();
-		
-		
-		listener = new ServerSocket();
-		listener.setReuseAddress(true);
-		InetAddress serverIP = InetAddress.getByName(serverAddress);
-		
-		listener.bind(new InetSocketAddress(serverIP,serverPort));
-		System.out.format("the server is running %s:%d%n",serverAddress, serverPort);
-		
-		//loading the dataBase
-		loadDatabaseFromCSV();
-		
-		try 
-		{
-			while(true) 
-			{
-				new ClientHandler(listener.accept(),clientNumber++).start();
-			}
+		try {
+			listener = new ServerSocket();
+			listener.setReuseAddress(true);
+			InetAddress serverIP = InetAddress.getByName(serverAddress);
+
+			listener.bind(new InetSocketAddress(serverIP, serverPort));
+			System.out.format("the server is running %s:%d%n", serverAddress, serverPort);
+		} catch (IOException e) {
+			System.out.println("Could not create server socket");
+			e.printStackTrace();
+			return;
 		}
-		finally 
-		{
+	}
+
+	public static void main(String[] args) throws Exception {
+		int clientNumber = 0;
+
+		// creating the server
+		creatingServer();
+
+		// loading the dataBase
+		loadDatabaseFromCSV();
+
+		// waiting for clients
+		try {
+			while (true) {
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+		} finally {
 			listener.close();
 		}
 	}
-	
-	
-	public static class ClientHandler extends Thread 
-	{
-		private Socket socket ;
-		private int clientNumber ;
-		
-		public ClientHandler(Socket socket, int clientNumber)
-		{
-			this.socket = socket ;
-			this.clientNumber = clientNumber ;
-			System.out.println("New connection with client#" + clientNumber + " at "+ socket);
+
+	public static class ClientHandler extends Thread {
+		private Socket socket;
+		private int clientNumber;
+		private DataInputStream in;
+		private DataOutputStream out;
+
+		public ClientHandler(Socket socket, int clientNumber) {
+			this.socket = socket;
+			this.clientNumber = clientNumber;
+			System.out.println("New connection with client#" + clientNumber + " at " + socket);
 		}
-		
-		
+
 		public static void saveDatabaseToCSV() {
-		    try (PrintWriter writer = new PrintWriter(new FileWriter("user_database.csv"))) {
-		        for (Map.Entry<String, String> entry : usersDatabase.entrySet()) {
-		            writer.println(entry.getKey() + "," + entry.getValue());
-		        }
-		        System.out.println("Database saved to CSV.");
-		    } catch (IOException e) {
-		        System.err.println("Error saving database to CSV: " + e.getMessage());
-		    }
+			try (PrintWriter writer = new PrintWriter(new FileWriter("user_database.csv"))) {
+				for (Map.Entry<String, String> entry : usersDatabase.entrySet()) {
+					writer.println(entry.getKey() + "," + entry.getValue());
+				}
+				System.out.println("Database saved to CSV.");
+			} catch (IOException e) {
+				System.err.println("Error saving database to CSV: " + e.getMessage());
+			}
 		}
-		
-		
-		public void run() 
-		{
-			try 
-			{
-//				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-//				out.writeUTF("Hello from server - you are client#" + clientNumber);	
-				
-				DataInputStream in = new DataInputStream(socket.getInputStream());
-				String username = in.readUTF();
-				String password = in.readUTF();
-				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-				
-				//verifier si le nom utilisateur existe dans la base de donnees 
-				if(!usersDatabase.containsKey(username)) {
-					//Creation d'un nouveau utilisateur
-					
-					usersDatabase.put(username, password);
-					
-					//saving user iformations in the CSV file
-					saveDatabaseToCSV();
-					
-					out.writeUTF("NOUVEAU");
-				}
-				else {
-					boolean isValidUser = usersDatabase.containsKey(username) &&
-					        usersDatabase.get(username).equals(password);
-					if(isValidUser) {
-						out.writeUTF("SUCCEE");
-					}
-					else {
-						out.writeUTF("ECHEC");
-					}
-				}
-				// Read the image file name sent by the client
-	            String imageName = in.readUTF();
-	            System.out.println("Received image name: " + imageName);
 
-	            // Read the image data sent by the client
-	            int imageDataLength = in.readInt();
-	            byte[] imageData = new byte[imageDataLength];
-	            in.readFully(imageData);
+		public String[] getUserinfo() {
 
-	            // Save the image data to a file
-	            FileOutputStream fos = new FileOutputStream(imageName);
-	            fos.write(imageData);
-	            fos.close();
-				
+			String[] result = null;
+
+			try {
+				in = new DataInputStream(socket.getInputStream());
+				result = new String[2];
+				result[0] = in.readUTF();
+				result[1] = in.readUTF();
+			} catch (IOException e) {
+				System.out.println("Could not read an answer from the client");
+				e.printStackTrace();
+				return null;
 			}
-			
-			catch (IOException e )
-			{
-				System.out.println("Error handling client#" + clientNumber + " : " + e);
-				
-			}
-			finally 
-			{
-				try 
-				{
-					socket.close();
-				}
-				catch (IOException e) 
-				{
-					System.out.println("Could not close socket") ;
-				}
-				
-				System.out.println("Connection with client#" + clientNumber + " closed");
-			}
-			
+			return result;
 		}
+
+		public void searchUser(String username, String password) {
+
+			String messagetoClient = null;
+
+			// verify if the user is already in the database
+			if (!usersDatabase.containsKey(username)) {
+
+				// Creating new user
+				usersDatabase.put(username, password);
+				messagetoClient = "NEW";
+				// saving user iformations in the CSV file
+				saveDatabaseToCSV();
+			} else {
+				// verify the password
+				String storedPassword = usersDatabase.get(username);
+				if (storedPassword.equals(password)) {
+					messagetoClient = "SUCCESS";
+				} else {
+					messagetoClient = "FAILED";
+				}
+
+			}
+			try {
+				out = new DataOutputStream(socket.getOutputStream());
+				out.writeUTF(messagetoClient);
+			} catch (IOException e) {
+				System.out.println("Could not send confirmation to client");
+			}
+		}
+
+		// this function is used to print the image and user informations received from
+		// the client
+		private void terminalReceivedMessage(String username, String imageName) {
+			System.out
+					.println("[" + username + " - " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort() + " - "
+							+ new SimpleDateFormat("yyyy-MM-dd@HH:mm:ss").format(new Date()) + "]" + " : Image " + imageName
+							+ " received for processing");
+
+		}
+
+		// extract the image name and the image data from the client
+		private Object[] readImage(String username) {
+			// Read the image data sent by the client
+			Object[] result = new Object[2];
+			try {
+				String imageName = in.readUTF();
+				result[0] = imageName;
+				terminalReceivedMessage(username, imageName);
+
+				int imageDataLength = in.readInt();
+				byte[] imageData = new byte[imageDataLength];
+				in.readFully(imageData);
+				result[1] = imageData;
+			} catch (IOException e) {
+				System.out.println("Could not read the image data sent by the client");
+				e.printStackTrace();
+			}
+			return result;
+		}
+
+		// this function is used to send the sobel image to the client
+		private void sendSobelImage(String imageName, byte[] imageData) {
+			try {
+				// Convert byte array to BufferedImage
+				BufferedImage bufferedImage = byteArrayToBufferedImage(imageData);
+
+				BufferedImage sobelImage = Sobel.process(bufferedImage);
+				String sobelImageName = "sobel_" + imageName;
+				out.writeUTF(sobelImageName);
+				// Convert Sobel image to byte array
+				byte[] sobelImageData = bufferedImageToByteArray(sobelImage);
+				int sobelImageDataLength = sobelImageData.length;
+
+				// Send the length of the Sobel image data first
+				out.writeInt(sobelImageDataLength);
+
+				// Send the Sobel image data
+				out.write(sobelImageData);
+
+			} catch (IOException e) {
+				System.out.println("Could not send the image to  client");
+				e.printStackTrace();
+				return;
+			}
+
+		}
+
+		// this function is used to hanlde all image operations between the server and
+		// the client
+		private void imageHandler(String username) {
+			Object[] imageData = readImage(username);
+
+			// Send the Sobel image to the client
+			sendSobelImage((String) imageData[0], (byte[]) imageData[1]);
+		}
+
+		private byte[] bufferedImageToByteArray(BufferedImage image) {
+			try {
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ImageIO.write(image, "jpg", baos);
+				return baos.toByteArray();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		private BufferedImage byteArrayToBufferedImage(byte[] imageData) {
+			try {
+				ByteArrayInputStream bis = new ByteArrayInputStream(imageData);
+				return ImageIO.read(bis);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		public void run() {
+
+			String[] userInfo = getUserinfo();
+			searchUser(userInfo[0], userInfo[1]);
+
+			imageHandler(userInfo[0]);
+
+			try {
+				socket.close();
+			} catch (IOException e) {
+				System.out.println("Could not close socket");
+			}
+
+			System.out.println("Connection with client#" + clientNumber + " closed");
+		}
+
 	}
-	
 }
-
